7fc1ea838fed8bc2d56d8d7d4e5bc345240fd6c7
Hannah Robinson <hannah1robinson1@gmail.com>
2024-12-29 14:53:27 -0700

    One More push before full api

---
+++ components/footer.js
@@ -0,0 +1,10 @@
+import html from "html-literal";
+
+export default () => html`
+  <footer>
+    <div class="help-button">
+      <h6>help</h6>
+      <a href="#"><i class="fa-solid fa-question"></i></a>
+    </div>
+  </footer>
+`;
